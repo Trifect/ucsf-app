f8a375cfc2757b43b47962045e8802c7b54d4fe5
Jung Kim <kjungsoo79@gmail.com>
2016-04-06 13:04:17 -0700

    Initial file add

---
+++ UCSF/UCSF/POST.swift
@@ -0,0 +1,11 @@
+//
+//  POST.swift
+//  UCSF
+//
+//  Created by Jisoo Kim on 4/6/16.
+//  Copyright © 2016 Jimbus. All rights reserved.
+//
+
+import Foundation
+
+//send post to serverapi.swift
---
+++ UCSF/UCSF/ServerAPI.swift
@@ -0,0 +1,12 @@
+//
+//  ServerAPI.swift
+//  UCSF
+//
+//  Created by Jisoo Kim on 4/6/16.
+//  Copyright © 2016 Jimbus. All rights reserved.
+//
+
+import Foundation
+
+
+//mock up server
