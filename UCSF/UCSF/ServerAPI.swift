c86885cd5fd113fd666214c183817e675ee6a2e0
Jung Kim <kjungsoo79@gmail.com>
2016-04-07 09:50:12 -0700

    Removed some faulty code in serverAPI.swift

--- UCSF/UCSF/ServerAPI.swift
+++ UCSF/UCSF/ServerAPI.swift
@@ -23,26 +23,6 @@ class MockServer { //how to structure this server???
     
     func getPost(json: NSString) {
         //parse into users, userIDs and hospitals
-        let data = json.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
-        do {
-            let data = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
-            if let dictionary = data as? [String: AnyObject] {
-                readJSONobject(dictionary)
-            }
-        } catch {
-            print("error") //exception handling goes here, "error" for now; need sophisticated exception handling implementation
-        }
-    }
-    
-    func readJSONobject(object: [String: AnyObject]) { //break into smaller dictionaries
-        guard let sessionID = object["FormSessionID"] as? String,
-            let trainee = object["Questions"]!["QID2"]!!["Value"] as? String,
-            let caseID = object["Questions"]!["QID3"]!!["Value"] as? String else { return }
-        
-        users.append(trainee)
-        userIDs["\(trainee)"] = caseID
-        sessionIDs["\(trainee)"] = sessionID
-        //send these to plist
     }
     
     func reply() {
