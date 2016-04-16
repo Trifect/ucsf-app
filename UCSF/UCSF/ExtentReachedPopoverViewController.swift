5f6fed54c5d678255c21201903e9f4793e16a493
robertposada <rep2@stmarys-ca.edu>
2016-04-15 17:12:14 -0700

    Added extent reached page of form

---
+++ UCSF/UCSF/ExtentReachedPopoverViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ExtentReachedPopoverViewController.swift
+//  UCSF
+//
+//  Created by Robert Posada on 4/14/16.
+//  Copyright © 2016 Jimbus. All rights reserved.
+//
+
+import UIKit
+
+var extentReachedList = ["Rectum", "Sigmoid", "Descending", "Transverse", "Ascending", "Cecum", "Terminal lleum"]
+
+class ExtentReachedPopoverViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var extentReachedTableView: UITableView!
+    
+    var passBackDelegate: PassBackDelegate! = nil
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return extentReachedList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.extentReachedTableView.dequeueReusableCellWithIdentifier("eCell", forIndexPath: indexPath) as! ExtentReachedCell
+        
+        cell.extentReachedbyTrainee.text = extentReachedList[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let returnString = extentReachedList[indexPath.row]
+        passBackDelegate.sendString(returnString)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
